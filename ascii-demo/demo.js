84131f9d7934e5308819491b0a8c892e6ce22bd8
Anthonny Quérouil <anthonny.querouil@gmail.com>
2014-02-19 10:49:36 +0100

    Add default content for people who don't know AsciiDoctor

--- ascii-demo/demo.js
+++ ascii-demo/demo.js
@@ -5,12 +5,18 @@ angular.module('demo', ['aql.asciidoc', 'ui.ace'])
 // Define Opal attributes and options
 .constant('asciidocOpts', Opal.hash2(['options'], {'header_footer': true}))
 
-.controller('demo', ['asciidocOpts', function(asciidocOpts){
+.controller('demo', ['asciidocOpts', '$http', function(asciidocOpts, $http){
+	var app = this;
+	$http({method: 'GET', url: 'demo.asciidoc'}).
+		success(function(data, status, headers, config) {
+			app.ascii = data;
+	  	});
 
-	this.asciidocOpts = asciidocOpts;
+
+	app.asciidocOpts = asciidocOpts;
 
 	// The ui-ace option
-	this.aceOption = {
+	app.aceOption = {
 		theme:'terminal',
 		mode: 'asciidoc'
 	}
