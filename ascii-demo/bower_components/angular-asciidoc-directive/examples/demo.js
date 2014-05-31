3405a5f992b3b911ca057d43d15ed8f1ef24d395
Anthonny Quérouil <anthonny.querouil@gmail.com>
2014-06-01 01:43:58 +0200

    Update main demo with 1.5.0-preview.7 Remove demo 1.5.0-preview4 and 1.5.0-preview.5

--- ascii-demo/bower_components/angular-asciidoc-directive/examples/demo.js
+++ ascii-demo/bower_components/angular-asciidoc-directive/examples/demo.js
@@ -1,43 +1,35 @@
 'use strict';
 
-angular.module('demo', ['aql.asciidoc', 'ui.ace'])
+angular.module('demo', ['aql.asciidoc', 'ui.ace', 'aql.directives.dropfile'])
 
 // Define Opal attributes and options
-.constant('asciidocOpts', Opal.hash2(['options'], {'header_footer': true}))
+.constant('asciidocOpts', Opal.hash2(['safe'], {'safe': 'unsafe'}))
 
-.controller('demo', ['asciidocOpts', '$http', function(asciidocOpts, $http){
+.controller('demo', ['asciidocOpts', function(asciidocOpts){
 	var app = this;
-
-	$http({method: 'GET', url: 'demo.asciidoc'}).
-		success(function(data, status, headers, config) {
-			app.ascii = data;
-	  	});
-
 
 	app.asciidocOpts = asciidocOpts;
 
+	app.ascii= '== Write some text or drop an AsciiDoc file in editor area\n';
+	app.ascii+= '= <-';
+
 	/**
-	 * Define transforme to change html generated with asciidoc
+	 * Define Post processor to change html generated with asciidoc
 	 * @param  {angular.element} element [description]
 	 * @return {html} html updated
 	 */
-	var urlImages = 'https://raw2.github.com/asciidoctor/asciidoctor.js/master/examples/';
-	var urlLink = 'https://github.com/Nikku/asciidoc-samples/blob/master/';
 
-	app.asciiTransformer = function(element) {
-		element.find('a').not('[href^="http"]').each(function() {
-			var el = angular.element(this)
-			var href = el.attr('href');
-			el.attr('href', urlLink+href)
-		});
+	app.asciiPostProcessor = function(element) {
 
-		element.find('img').not('[src^="http"]').each(function() {
+		element.find('a[href^="#"]').each(function() {
 			var el = angular.element(this);
-			var srcImg = el.attr('src');
-			el.attr('src',  urlImages+srcImg);
+			el.on('click', function(){
+				$location.hash(el.attr('href'));
+      	$anchorScroll();
+			});
 		});
-
 		return element;
+		
 	}
 
 	// The ui-ace option
@@ -45,4 +37,19 @@ angular.module('demo', ['aql.asciidoc', 'ui.ace'])
 		theme:'terminal',
 		mode: 'asciidoc'
 	}
+
+
+
+	app.onDragOver = function(event) {
+		console.log(event);
+	};
+
+	app.onDragEnter = function(event) {
+		console.log(event);
+	};
+
+	app.onDragLeave = function(event) {
+		console.log(event);
+	};
+
 }]);
